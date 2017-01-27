642383daadad51218c670ce7c73b69f3212ad334
leloz suliman <leloz997@gmail.com>
2017-01-27 09:20:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
-end
+  #attr_reader :x, :o, :y
+  def initialize(x, o, y)
+    @x = x
+    @o = o
+    @y = y
+  end
+
+  def kind
+    legal_triangle
+    if @x == @o && @o == @y
+      :equilateral
+    elsif @x == @o || @o == @y || @x == @y
+      :isosceles
+    else
+      :scalene
+    end
+     end
+
+  def legal_triangle
+     if (@x + @o + @y != 0) && (@x + @o > @y) && (@x + @y > @o) && (@o + @y > @x)
+  else
+    raise TriangleError
+  end
+   end
+    end
+  class TriangleError < StandardError
+   end 
